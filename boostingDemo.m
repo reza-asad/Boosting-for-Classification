62a8f95e7c2f64f330a8d17106b6a6fdea158903
Reza <reza.asad@math.ubc.ca>
2014-12-24 14:50:10 -0800

    Create boostingDemo.m

---
+++ boostingDemo.m
@@ -0,0 +1,33 @@
+clear all
+clear all;
+clc;
+close all
+
+load binary.mat
+N = size(X,1);
+
+% Fit Single model
+fprintf('Fitting Single model...\n');
+z = (1/N)*ones(N,1);
+% model = logisticRegress(X,y,z);
+model = decisionStump(X,y,z);
+
+% Compute training error
+yhat = model.predict(model,X);
+trainingError = sum(yhat ~= y)/N
+
+% Show data and decision boundaries
+plot2DClassifier(X,y,model);
+pause
+% Fit Boosted model
+fprintf('Fitting boosted model...\n');
+nBoosts = 50;
+% model = adaBoost(X,y,nBoosts,@logisticRegress);
+model = adaBoost(X,y,nBoosts,@decisionStump);
+
+% Compute training error
+yhat = model.predict(model,X);
+trainingError = sum(yhat ~= y)/N
+
+% Show data and decision boundaries
+plot2DClassifier(X,y,model);
